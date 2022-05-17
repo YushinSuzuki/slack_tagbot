ed7ba0100ed7b42e8a477aae90833effccb3c389
YushinSuzuki808inc <suzuki_yushin@808inc.net>
2022-05-18 00:03:18 +0900

    second commit

--- app.js
+++ app.js
@@ -57,9 +57,8 @@ app.event('message', async({ event, client, logger }) => {
                 var parent_messages;
                 new_parent_message;
 
-                const start_idx2 = replies.messages[0].text.indexOf("<#")
-                const end_idx2 = replies.messages[0].text.indexOf("|")
-                const parent_origin_text = replies.messages[0].text.substr(start_idx + 2, end_idx - 2);
+                const text_idx2 = last_mes.text.indexOf(">")
+                const parent_origin_text = replies.messages[0].text.substr(text_idx + 2);
                 const parent_ts = replies.messages[0].ts.replace('.', '');
 
                 const parent_text = `<https://test.slack.com/archives/${event_channell}/p${parent_ts}|${parent_origin_text}> `
