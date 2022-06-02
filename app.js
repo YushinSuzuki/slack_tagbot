adff0002a9b181e8705f015cc585371ef5e3ab9c
YushinSuzuki808inc <suzuki_yushin@808inc.net>
2022-06-03 02:20:40 +0900

    second commit

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ const app = new App({
 receiver.router.get('/slack/user_install', async(_req, res) => {
     try {
         // feel free to modify the scopes
-        const url = await receiver.installer ? .generateInstallUrl({
+        const url = await receiver.installer.generateInstallUrl({
             scopes: ['chat:write', 'users.profile:read', 'channels:history', 'channels:read', 'groups:history', 'groups:read', 'im:history', 'mpim:history'],
             userScopes: ['chat:write', 'users.profile:read', 'channels:history', 'channels:read', 'groups:history', 'groups:read', 'im:history', 'mpim:history'],
         });
@@ -52,7 +52,7 @@ receiver.router.get('/slack/user_install', async(_req, res) => {
     }
 });
 
-const buildSlackUrl = (url) =>
+const buildSlackUrl = (url) => {
     `<a href=${url}><img alt=""Add to Slack"" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>`;
 }
 
